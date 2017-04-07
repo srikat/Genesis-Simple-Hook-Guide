ef072ca1bc1fbc1be64e2e67a056753184cb121c
christophherr <info@christophherr.com>
2017-04-07 13:27:53 -0400

    Replaces onclick with event listener

--- genesis-simple-hook-guide.php
+++ genesis-simple-hook-guide.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Genesis Simple Hook Guide
  * Plugin URI: https://github.com/srikat/Genesis-Simple-Hook-Guide
@@ -20,106 +21,160 @@
  *
  */
 
+
 if ( ! defined( 'WPINC' ) ) {
+
 	die;
+
 }
 
+
 register_activation_hook( __FILE__, 'gshg_activation_check' );
+
+
 /**
  * Check if Genesis is the parent theme.
  */
+
 function gshg_activation_check() {
+
 	$theme_info = wp_get_theme();
 
+
 	$genesis_flavors = array(
-		'genesis',
-		'genesis-trunk',
+	'genesis',
+	'genesis-trunk',
 	);
 
+
 	if ( ! in_array( $theme_info->Template, $genesis_flavors, true ) ) {
-		deactivate_plugins( plugin_basename( __FILE__ ) ); // Deactivate ourself.
+
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+		// 		Deactivate ourself.
 		$message = sprintf(
-			/* translators: %s: URL to Genesis Framework. */
-			__( 'Sorry, you can\'t activate this plugin unless you have installed <a href="%s">Genesis</a>.', 'genesis-simple-hook-guide' ),
-			esc_url( 'https://my.studiopress.com/themes/genesis/' )
+
+		/* translators: %s: URL to Genesis Framework. */
+
+		__( 'Sorry, you can\'t activate this plugin unless you have installed <a href="%s">Genesis</a>.', 'genesis-simple-hook-guide' ),
+		esc_url( 'https://my.studiopress.com/themes/genesis/' )
 		);
+
 		wp_die( $message );
+
 	}
+
 }
 
+
 add_action( 'admin_bar_menu', 'gshg_admin_bar_links', 100 );
+
+
 /**
  * Add admin bar items.
  */
+
 function gshg_admin_bar_links() {
+
 	global $wp_admin_bar;
 
+
 	if ( is_admin() || false === function_exists( 'genesis' ) ) {
+
 		return;
+
 	}
 
+
 	$wp_admin_bar->add_menu(
-		array(
-			'id' => 'gshg',
-			'title' => __( 'Genesis Hooks', 'genesis-simple-hook-guide' ),
-			'href' => '',
-			'position' => 0,
-		)
+	array(
+	'id' => 'gshg',
+	'title' => __( 'Genesis Hooks', 'genesis-simple-hook-guide' ),
+	'href' => '',
+	'position' => 0,
+	)
 	);
+
 	$wp_admin_bar->add_menu(
-		array(
-			'id'	   => 'gshg_action',
-			'parent'   => 'gshg',
-			'title'    => __( 'Action Hooks', 'genesis-simple-hook-guide' ),
-			'href'     => esc_url( add_query_arg( 'gshg_hooks', 'show' ) ),
-			'position' => 10,
-		)
+	array(
+	'id'	   => 'gshg_action',
+	'parent'   => 'gshg',
+	'title'    => __( 'Action Hooks', 'genesis-simple-hook-guide' ),
+	'href'     => esc_url( add_query_arg( 'gshg_hooks', 'show' ) ),
+	'position' => 10,
+	)
 	);
+
 	$wp_admin_bar->add_menu(
-		array(
-			'id'	   => 'gshg_clear',
-			'parent'   => 'gshg',
-			'title'    => __( 'Clear', 'genesis-simple-hook-guide' ),
-			'href'     => esc_url( remove_query_arg(
-				array(
-					'gshg_hooks',
-				)
-			) ),
-			'position' => 10,
-		)
+	array(
+	'id'	   => 'gshg_clear',
+	'parent'   => 'gshg',
+	'title'    => __( 'Clear', 'genesis-simple-hook-guide' ),
+	'href'     => esc_url( remove_query_arg(
+	array(
+	'gshg_hooks',
+	)
+	) ),
+	'position' => 10,
+	)
 	);
+
 
 }
 
+
 // add_action( 'admin_enqueue_scripts', 'gshg_hooks_stylesheet' );
-add_action( 'wp_enqueue_scripts', 'gshg_hooks_stylesheet' );
+
+add_action( 'wp_enqueue_scripts', 'gshg_hooks_script_and_styles' );
+
+
 /**
  * Load stylesheet.
  */
-function gshg_hooks_stylesheet() {
+
+function gshg_hooks_script_and_styles() {
+
 	$gshg_plugin_css_url = plugins_url( 'style.css', __FILE__ );
+	$gshg_plugin_js_url = plugins_url( 'general.js', __FILE__ );
 
 	if ( 'show' === filter_input( INPUT_GET, 'gshg_hooks', FILTER_SANITIZE_STRING ) ) {
+
 		wp_enqueue_style( 'gshg-styles', $gshg_plugin_css_url );
+		wp_enqueue_script( 'gshg-scripts', $gshg_plugin_js_url, array( 'jquery' ) );
+
 	}
+
 }
 
 add_action( 'all', 'gshg_print_hooks_on_page' );
+
+
 /**
  * Print the hooks.
  */
+
 function gshg_print_hooks_on_page() {
-	// BAIL without hooking into anything if on the admin page or if not displaying anything.
+
+	// 	BAIL without hooking into anything if on the admin page or if not displaying anything.
 	if ( is_admin() || ! ( 'show' === filter_input( INPUT_GET, 'gshg_hooks', FILTER_SANITIZE_STRING ) ) ) {
+
 		return;
+
 	}
+
 
 	global $wp_actions;
+
 	$filter = current_filter();
 
+
 	if ( 'genesis_' === substr( $filter, 0, 8 ) ) {
+
 		if ( isset( $wp_actions[ $filter ] ) ) {
-			printf( '<div id="%1$s" class="genesis-hook"><input type="text" readonly onclick="jQuery(this).select();" value="%1$s" /></div>', $filter );
+
+			printf( '<div id="%1$s" class="genesis-hook"><input type="text" readonly value="%1$s" /></div>', $filter );
+
 		}
+
 	}
+
 }
